33f3a09ddacadcd1e7ae1f5066dd8a528de74276
agagliano <agagliano@redseal.co>
2016-05-29 20:46:02 -0700

    section 3, lecture 22-26.
     example buttons added with:
    
     1.  class binding
     2.  style binding
     3.  event binding

--- app/app.component.js
+++ app/app.component.js
@@ -24,14 +24,31 @@ System.register(['@angular/core', './courses.component', './authors.component'],
                 authors_component_1 = authors_component_1_1;
             }],
         execute: function() {
+            // import {ButtonsDemoComponent} from "./button.component";
             AppComponent = (function () {
                 function AppComponent() {
+                    this.isBlue = true;
                 }
+                /*
+                 * Section 3, lesson 24 -- event binding.
+                 *
+                 * example of event binding, see the button object.
+                 *
+                 * The following binds teh click event from the button to this method:
+                 *
+                 *                      (click)="onClick($event)"
+                 *
+                 * Also note, $event is passed from the UI ot back end so the event can be inspected.
+                 * This is standard jquery, not angular, angular just allows this by passing '$event'
+                 */
+                AppComponent.prototype.onClick = function ($event) {
+                    console.log('button was clicked!', event);
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n<h1>My First Angular 2 App</h1>\n<courses></courses> <!-- added from lessons -->\n<authors></authors> <!-- added for exercise --> \n",
-                        directives: [courses_component_1.CoursesComponent, authors_component_1.AuthorsComponent]
+                        template: "\n<h1>My First Angular 2 App</h1>\n<courses></courses> <!-- added from lessons -->\n<authors></authors> <!-- added for exercise --> \n<!-- Section 3, lesson 23 -- class binding. (style.backgroundColor ...) below -->\n<button \n    class=\"btn btn-primary\"\n    [style.backgroundColor]=\"isBlue ? 'blue' : 'gray' \" \n    (click)=\"onClick($event)\">blue or gray</button>    \n<!-- <buttons-demo></buttons-demo> --> <!-- added for attempting to get bootstrap to work, not yet -->\n",
+                        directives: [courses_component_1.CoursesComponent, authors_component_1.AuthorsComponent,]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-
 /*
  * TOP level used by main.ts
  */
@@ -7,6 +6,7 @@ import {Component} from '@angular/core';
 // import components.
 import {CoursesComponent} from './courses.component'
 import {AuthorsComponent} from './authors.component'
+// import {ButtonsDemoComponent} from "./button.component";
 
 @Component({
     selector: 'my-app',
@@ -14,7 +14,32 @@ import {AuthorsComponent} from './authors.component'
 <h1>My First Angular 2 App</h1>
 <courses></courses> <!-- added from lessons -->
 <authors></authors> <!-- added for exercise --> 
+<!-- Section 3, lesson 23 -- class binding. (style.backgroundColor ...) below -->
+<button 
+    class="btn btn-primary"
+    [style.backgroundColor]="isBlue ? 'blue' : 'gray' " 
+    (click)="onClick($event)">blue or gray</button>    
+<!-- <buttons-demo></buttons-demo> --> <!-- added for attempting to get bootstrap to work, not yet -->
 `,
-    directives: [CoursesComponent, AuthorsComponent]
+    directives: [CoursesComponent, AuthorsComponent, /**ButtonsDemoComponent**/]
 })
-export class AppComponent { }
+
+export class AppComponent {
+    isBlue=true;
+
+    /*
+     * Section 3, lesson 24 -- event binding.
+     *
+     * example of event binding, see the button object.
+     *
+     * The following binds teh click event from the button to this method:
+     *
+     *                      (click)="onClick($event)"
+     *
+     * Also note, $event is passed from the UI ot back end so the event can be inspected.
+     * This is standard jquery, not angular, angular just allows this by passing '$event'
+     */
+    onClick($event){
+        console.log('button was clicked!', event)
+    }
+}
